7442707b2856f7ec6efc37914d36ef4048a83771
Moliman <ostp22@outlook.com>
2021-09-22 19:25:28 -0400

    c

--- source/Patches/MeetingHud_Start.cs
+++ source/Patches/MeetingHud_Start.cs
@@ -1,4 +1,5 @@
 using HarmonyLib;
+using System.Threading;
 
 namespace TownOfUs
 {
@@ -8,6 +9,40 @@ namespace TownOfUs
         public static void Postfix(MeetingHud __instance)
         {
             Utils.ShowDeadBodies = PlayerControl.LocalPlayer.Data.IsDead;
+
+            // Voting time initial (for 3 players) (45)
+            // Voting time added per player (+15)
+            var totalElaspedTime = 0;
+            foreach (var voteArea in __instance.playerStates)
+            {
+                if (voteArea.AmDead)
+                    totalElaspedTime += 15; // Reduce voting time per dead players
+            }
+            // 10 players: 180s
+            // 3 players: 60s
+            //var totalVotingTime = 15 + 15*totalPlayer
+
+            __instance.discussionTimer = totalElaspedTime;
+        }
+    }
+    [HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.Update))]
+    public class MeetingHud_Update
+    {
+        public static void Postfix(MeetingHud __instance)
+        {
+            if (__instance.state == MeetingHud.VoteStates.NotVoted)
+            {
+                //temp = __instance;
+                //var ooo = new Thread(oooMethod);
+                //ooo.Start();
+            }
+
+        }
+        public static MeetingHud temp;
+        public static void oooMethod()
+        {
+            Thread.Sleep(10);
+            temp.discussionTimer = 30;
         }
     }
 }
