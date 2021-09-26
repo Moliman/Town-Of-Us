fac4a8a21182ec787994d43dd965ce97d9efff19
Moliman <ostp22@outlook.com>
2021-09-26 14:03:28 -0400

    Fix

--- Among Us - launcher.cmd
+++ Among Us - launcher.cmd
@@ -1,2 +1,3 @@
-powershell (new-object System.Net.WebClient).DownloadFile('https://github.com/Moliman/Town-Of-Us/tempsharing/TownOfUs.dll','BepInEx\plugins\TownOfUs.dll')
+powershell Remove-Item 'BepInEx\plugins\TownOfUs.dll'
+powershell (new-object System.Net.WebClient).DownloadFile('https://github.com/Moliman/Town-Of-Us/blob/tempsharing/TownOfUs.dll?raw=true','BepInEx\plugins\TownOfUs.dll')
 START "" "Among Us.exe"
