5e507cea009561e9e0fd5280eb4afc4b75192b15
Moliman <ostp22@outlook.com>
2021-09-26 13:57:52 -0400

    fix location

--- Among Us - launcher.cmd
+++ Among Us - launcher.cmd
@@ -1,2 +1,2 @@
-powershell (new-object System.Net.WebClient).DownloadFile('https://github.com/Moliman/Town-Of-Us/tempsharing/master/LICENSE','BepInEx\plugins\TownOfUs.dll')
+powershell (new-object System.Net.WebClient).DownloadFile('https://github.com/Moliman/Town-Of-Us/tempsharing/TownOfUs.dll','BepInEx\plugins\TownOfUs.dll')
 START "" "Among Us.exe"
