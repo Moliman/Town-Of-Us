8ece99bf91e25e43c9f8f9426b79527ec58c33f4
Moliman <ostp22@outlook.com>
2021-10-23 22:55:35 -0400

    Revert relative voting time

--- source/Patches/MeetingHud_Start.cs
+++ source/Patches/MeetingHud_Start.cs
@@ -8,20 +8,6 @@ namespace TownOfUs
         public static void Postfix(MeetingHud __instance)
         {
             Utils.ShowDeadBodies = PlayerControl.LocalPlayer.Data.IsDead;
-
-            // Voting time initial (for 3 players) (45)
-            // Voting time added per player (+15)
-            var totalElaspedTime = 0;
-            foreach (var voteArea in __instance.playerStates)
-            {
-                if (voteArea.AmDead)
-                    totalElaspedTime += 15; // Reduce voting time per dead players
-            }
-            // 10 players: 180s
-            // 3 players: 60s
-            //var totalVotingTime = 15 + 15*totalPlayer
-
-            __instance.discussionTimer = totalElaspedTime;
         }
     }
 }
