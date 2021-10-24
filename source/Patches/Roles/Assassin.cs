ad2a7631711328687c388cff0b04ac95021fcaf6
Moliman <ostp22@outlook.com>
2021-10-23 22:38:36 -0400

    Assassin only see role that aren't set to 0%

--- source/Patches/Roles/Assassin.cs
+++ source/Patches/Roles/Assassin.cs
@@ -10,21 +10,8 @@ namespace TownOfUs.Roles
         public Dictionary<byte, (GameObject, GameObject, TMP_Text)> Buttons = new Dictionary<byte, (GameObject, GameObject, TMP_Text)>();
 
 
-        public Dictionary<string, Color> ColorMapping = new Dictionary<string, Color>
-        {
-            { "Mayor", new Color(0.44f, 0.31f, 0.66f, 1f) },
-            { "Sheriff", Color.yellow },
-            { "Engineer", new Color(1f, 0.65f, 0.04f, 1f) },
-            { "Swapper", new Color(0.4f, 0.9f, 0.4f, 1f) },
-            { "Investigator", new Color(0f, 0.7f, 0.7f, 1f) },
-            { "Time Lord", new Color(0f, 0f, 1f, 1f) },
-            { "Lover", new Color(1f, 0.4f, 0.8f, 1f) },
-            { "Medic", new Color(0f, 0.4f, 0f, 1f) },
-            { "Seer", new Color(1f, 0.8f, 0.5f, 1f) },
-            { "Spy", new Color(0.8f, 0.64f, 0.8f, 1f) },
-            { "Snitch", new Color(0.83f, 0.69f, 0.22f, 1f) },
-            { "Altruist", new Color(0.4f, 0f, 0f, 1f) }
-        };
+        public Dictionary<string, Color> ColorMapping = new Dictionary<string, Color>();
+
 
         public Dictionary<byte, string> Guesses = new Dictionary<byte, string>();
 
@@ -40,13 +27,47 @@ namespace TownOfUs.Roles
 
             RemainingKills = CustomGameOptions.AssassinKills;
 
+            if (CustomGameOptions.MayorOn != 0)
+                ColorMapping.Add("Mayor", new Color(0.44f, 0.31f, 0.66f, 1f));
+            if (CustomGameOptions.SheriffOn != 0)
+                ColorMapping.Add("Sheriff", Color.yellow);
+            if (CustomGameOptions.EngineerOn != 0)
+                ColorMapping.Add("Engineer", new Color(1f, 0.65f, 0.04f, 1f));
+            if (CustomGameOptions.SwapperOn != 0)
+                ColorMapping.Add("Swapper", new Color(0.4f, 0.9f, 0.4f, 1f));
+            if (CustomGameOptions.InvestigatorOn != 0)
+                ColorMapping.Add("Investigator", new Color(0f, 0.7f, 0.7f, 1f));
+            if (CustomGameOptions.TimeLordOn != 0)
+                ColorMapping.Add("Time Lord", new Color(0f, 0f, 1f, 1f));
+            if (CustomGameOptions.LoversOn != 0)
+                ColorMapping.Add("Lover", new Color(1f, 0.4f, 0.8f, 1f));
+            if (CustomGameOptions.MedicOn != 0)
+                ColorMapping.Add("Medic", new Color(0f, 0.4f, 0f, 1f));
+            if (CustomGameOptions.SeerOn != 0)
+                ColorMapping.Add("Seer", new Color(1f, 0.8f, 0.5f, 1f));
+            if (CustomGameOptions.JesterOn != 0)
+                ColorMapping.Add("Spy", new Color(0.8f, 0.64f, 0.8f, 1f));
+            if (CustomGameOptions.SnitchOn != 0)
+                ColorMapping.Add("Snitch", new Color(0.83f, 0.69f, 0.22f, 1f));
+            if (CustomGameOptions.AltruistOn != 0)
+                ColorMapping.Add("Altruist", new Color(0.4f, 0f, 0f, 1f));
+
             if (CustomGameOptions.AssassinGuessNeutrals)
             {
-                ColorMapping.Add("Jester", new Color(1f, 0.75f, 0.8f, 1f));
-                ColorMapping.Add("Shifter", new Color(0.6f, 0.6f, 0.6f, 1f));
-                ColorMapping.Add("Executioner", new Color(0.55f, 0.25f, 0.02f, 1f));
-                ColorMapping.Add("The Glitch", Color.green);
-                ColorMapping.Add("Arsonist", new Color(1f, 0.3f, 0f));
+                if (CustomGameOptions.JesterOn != 0)
+                    ColorMapping.Add("Jester", new Color(1f, 0.75f, 0.8f, 1f));
+
+                if (CustomGameOptions.ShifterOn != 0)
+                    ColorMapping.Add("Shifter", new Color(0.6f, 0.6f, 0.6f, 1f));
+
+                if (CustomGameOptions.ExecutionerOn != 0)
+                    ColorMapping.Add("Executioner", new Color(0.55f, 0.25f, 0.02f, 1f));
+
+                if (CustomGameOptions.GlitchOn != 0)
+                    ColorMapping.Add("The Glitch", Color.green);
+
+                if (CustomGameOptions.ArsonistOn != 0)
+                    ColorMapping.Add("Arsonist", new Color(1f, 0.3f, 0f));
             }
 
             if (CustomGameOptions.AssassinCrewmateGuess) ColorMapping.Add("Crewmate", Color.white);
